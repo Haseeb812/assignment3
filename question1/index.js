40aa4d18c906f37e0a36e71c10228610d0d59017
haseeb812 <haseeb.expert007@gmail.com>
2024-08-16 17:33:22 +0500

    Add <file_name> to the repository

---
+++ question1/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// declare a word and assignment my age to it in type number annotation also assign a number to it.
+// const myAge: number = 29;
+// let fullName: string = "Riwan Gulzar";
+// console.log(`my name is ${fullName} and i am ${myAge} years old`);
